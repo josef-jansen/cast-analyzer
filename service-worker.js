9572b9dbf8d941e3f2688dbddc03b80ccbffe864
josef-jansen <67290670+josef-jansen@users.noreply.github.com>
2026-04-19 17:11:28 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-// Lege service worker — versie 1
-self.addEventListener("install", () => {
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", () => {
-    // Geen caching, geen intercept — volledig veilig
-});
